a74961a6015ea4dbd27673e5e8957000d4ed0216
AgustinDiLeone <agustindileone@gmail.com>
2023-01-31 12:15:04 -0300

    nombres

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,11 @@
-/*
+/* Agustin Di leone
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	var nombreIngresado; //declaracion de variable
+	nombreIngresado = txtIdNombre.value;
+	alert("nombreIngresado");
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,4 +1,4 @@
-/*
+/*Agustin Di leone
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
