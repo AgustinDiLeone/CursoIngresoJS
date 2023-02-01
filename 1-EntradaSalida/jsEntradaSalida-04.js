6d9a37fde4ea07faf58c2f4bc25be8f3de4615d2
AgustinDiLeone <agustindileone@gmail.com>
2023-02-01 10:03:26 -0300

    listo

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -5,16 +5,16 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 function mostrar()
 {
 	//declaro variable
-	let datoIngresado 
-	let cajita
+	let datoIngresado;
+	let cajita;
 	
 	//pido el dato
-	datoIngresado = prompt ("Ingresar datos") 
+	datoIngresado = prompt ("Ingresar datos");
 
-	cajita = datoIngresado
+	cajita = datoIngresado;
 
 	//Acceder a la cajita                             Dentro de la caja
-	document.getElementById("txtIdNombre").value = cajita
+	document.getElementById("txtIdNombre").value = cajita;
 
   // ENTRADA mostrarDato = document.getElementById("txtIdNombre").value;
   // SALIDA document.getElementById("txtIdNombre").value; = mostrarDato 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,14 +3,14 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {
-	let nombrePersona
-    let edadPersona
-    let mensaje
+	let nombrePersona;
+    let edadPersona;
+    let mensaje;
 
-	nombrePersona = document.getElementById("txtIdNombre").value
-	edadPersona = document.getElementById("txtIdEdad").value
+	nombrePersona = document.getElementById("txtIdNombre").value;
+	edadPersona = document.getElementById("txtIdEdad").value;
 	
-	mensaje = "Usted se llama " + nombrePersona + " y tiene " + edadPersona + " años"
+	mensaje = "Usted se llama " + nombrePersona + " y tiene " + edadPersona + " años";
 	alert(mensaje);
 }
 
