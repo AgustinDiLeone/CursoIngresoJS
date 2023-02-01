5326605b7275f3e7bfd8b6d36f4b9206b5b15f59
AgustinDiLeone <agustindileone@gmail.com>
2023-02-01 15:20:20 -0300

    listo

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,18 +1,56 @@
-/*2.	Para el departamento de Construcción:
-A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
+/*2. Agustin Di Leone	
+Para el departamento de Construcción:
+A.	mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
 function Rectangulo () 
 {
+    let alambreLargo;
+    let alambreAncho;
+    let resultado;
+    let mensaje;
 
+    alambreLargo = document.getElementById("txtIdLargo").value;
+    alambreAncho = document.getElementById("txtIdAncho").value;
+    alambreLargo = parseInt(alambreLargo);
+    alambreAncho = parseInt(alambreAncho);
+    resultado = ((alambreLargo*2)+(alambreAncho*2))*3;
+
+    alert("Se necesitara " +resultado+ " metros de alambre ");
+   
 }
+
 function Circulo () 
-{
-	
+{ 
+    let alambreRadio;
+    let resultado;
+    let mensaje;
+
+    alambreRadio = document.getElementById("txtIdRadio").value;
+    alambreRadio = parseInt(alambreRadio);
+    resultado = 3.14*2*alambreRadio;
+
+    alert("Se necesitaran " + resultado + " metros de alambre");
 }
 function Materiales () 
 {
+    let alambreLargo;
+    let alambreAncho;
+    let superficie;
+    let resultadoCemento;
+    let resultadoCal
+    let mensaje;
+
+    terrenoLargo = document.getElementById("txtIdLargo").value;
+    terrenoAncho = document.getElementById("txtIdAncho").value;
+    terrenoLargo = parseInt(terrenoLargo);
+    terrenoAncho = parseInt(terrenoAncho);
+    superficie = terrenoAncho*terrenoLargo;
+    resultadoCemento = superficie*2;
+    resultadoCal = superficie*3;
+
+    alert("Se necesitaran " +resultadoCemento+ " bolsas de cemento y " +resultadoCal +" bolsas de cal");
 	
 }
