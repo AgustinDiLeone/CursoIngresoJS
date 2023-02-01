ced274e26976bc80e99e6be905210cee5e7cda87
AgustinDiLeone <agustindileone@gmail.com>
2023-02-01 11:19:37 -0300

    listo

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -4,22 +4,85 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	alert("ok");	
+{
+	let numUno;
+	let numDos;
+	let datoUno;
+	let datoDos;
+	let resultado;
+	let mensaje;
+
+	datoUno = document.getElementById("txtIdNumeroUno").value;
+	datoDos = document.getElementById("txtIdNumeroDos").value;
+
+	numUno = parseInt(datoUno);
+	numDos = parseInt(datoDos);
+	resultado = numUno + numDos;
+	mensaje = 'El resultado es ' + resultado;
+
+	alert( mensaje);	
 }
 
 function restar()
 {
+	let numUno;
+	let numDos;
+	let datoUno;
+	let datoDos;
+	let resultado;
+	let mensaje;
+
+	datoUno = document.getElementById("txtIdNumeroUno").value;
+	datoDos = document.getElementById("txtIdNumeroDos").value;
+
+	numUno = parseInt(datoUno);
+	numDos = parseInt(datoDos);
+	resultado = numUno - numDos;
+	mensaje = 'El resultado es ' + resultado;
+	
+	alert( mensaje);
 	
 }
 
 function multiplicar()
 { 
+	let numUno;
+	let numDos;
+	let datoUno;
+	let datoDos;
+	let resultado;
+	let mensaje;
+
+	datoUno = document.getElementById("txtIdNumeroUno").value;
+	datoDos = document.getElementById("txtIdNumeroDos").value;
+
+	numUno = parseInt(datoUno);
+	numDos = parseInt(datoDos);
+	resultado = numUno * numDos;
+	mensaje = 'El resultado es ' + resultado;
+	
+	alert( mensaje);
 	
 }
 
 function dividir()
 {
+	let numUno;
+	let numDos;
+	let datoUno;
+	let datoDos;
+	let resultado;
+	let mensaje;
+
+	datoUno = document.getElementById("txtIdNumeroUno").value;
+	datoDos = document.getElementById("txtIdNumeroDos").value;
+
+	numUno = parseInt(datoUno);
+	numDos = parseInt(datoDos);
+	resultado = numUno / numDos;
+	mensaje = 'El resultado es ' + resultado;
+	
+	alert( mensaje);
 	
 }
 
