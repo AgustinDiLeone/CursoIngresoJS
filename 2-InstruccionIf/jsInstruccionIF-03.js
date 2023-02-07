51edca79945eb7014cd706babc0067d5bf127ac6
AgustinDiLeone <agustindileone@gmail.com>
2023-02-07 09:42:23 -0300

    listo

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -2,7 +2,18 @@
 
 function mostrar()
 {
-	
+	let edad;
+	let mensaje;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if (edad >= 18){
+
+		mensaje = "Sos mayor de edad"
+	}else{
+
+		mensaje = "Sos menor de edad"
+	}
 	alert(mensaje);
 
-}//FIN DE LA FUNCIÓN
+}
