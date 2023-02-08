00a4440dce23a912262a0e476280b3889e194ec0
AgustinDiLeone <agustindileone@gmail.com>
2023-02-08 12:29:05 -0300

    listo

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,7 +1,7 @@
 //Agustin Di Leone
 
 function mostrar()
-{
+/*{
 	let edad;
 	let mensaje;
 
@@ -14,17 +14,58 @@ function mostrar()
 	}
 	
 	alert(mensaje);
+}*/
+
+/*Ejercicio 1bis: 
+/con if
+pedir a una persona que fue de vaciones , 
+la distancia que viajo y el tiempo que tardo, para calcular la velocidad
+e informar:
+60 km/hr = muy lento 
+hata 80km/h= lento 
+hasta 100= buen ritmo
+hasta 120= ahi de la ley 
+mas = eso no se hace*/
+
+/*
+{
+let distancia;
+let tiempo;
+let velocidad;
+
+distancia = parseInt(document.getElementById("txtIdEdad").value);
+tiempo = parseFloat(prompt("¿cuanto tiempo tardaste?"));
+
+velocidad = distancia / tiempo;
+
+if (velocidad <= 60){
+	mensaje = "Muy lento";
+}else if (velocidad <= 80){
+	mensaje = "Lento";
+}else if(velocidad <= 100){
+	mensaje = "Buen ritmo";
+}else if(velocidad <= 120){
+	mensaje = "Ahi de la ley";
+}else{
+	mensaje = "Eso no se hace";
 }
-
-/* operadores relacionales o condicionales
-< menor
-> mayor
-<= menor o igual >=mayor o igual
-== igual
-!= distinto
-
-operadores logicos
-&& y logica
-|| o logica
-! negacion 
+alert(mensaje);
+}
 */
+{
+	/*Ejercicio 1 bis bis:
+	con if
+	ingresar el nombre y los datos requeridos para calcular el IMC , 
+	e informar a la persona si es:
+	Bajo peso <18.5
+	Peso normal 18,5-24.9
+	Preobesidad 25-26.9
+	Obesidad I 27-29.9
+	Obesidad II 30-34.9
+	Obesidad III >40
+	*/
+
+
+
+	
+}
