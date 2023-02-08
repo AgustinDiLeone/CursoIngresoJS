2d5f2e8d556e39f5c9cb77c61bcf797df961c777
AgustinDiLeone <agustindileone@gmail.com>
2023-02-08 10:05:34 -0300

    listo

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -3,9 +3,11 @@
 function mostrar()
 {
 	let x;
+	let max = 10;
+	let min = 1;
 
-	x = Math.floor(Math.random()*10)+1;
+	x = Math.floor(Math.random()*max + min);
 	
-	alert(x);	
+	console.log(x);	
 
 }
