1a03fdc98d73ee616a5b95bfb3c1b47dd43e59b7
AgustinDiLeone <agustindileone@gmail.com>
2023-02-09 12:26:36 -0300

    listo

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,27 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destinoIngresado;
+	let mensaje;
+
+	destinoIngresado = document.getElementById("txtIdDestino").value;
+
+	switch(destinoIngresado){
+		case"Bariloche":
+			mensaje = "Se encuentra al Sur de nuestro pais";
+			break;
+		case"Cataratas":
+			mensaje = "Se encuentra al Este de nuestro pais";
+			break;
+		case"Mar del plata":
+			mensaje = "Se encuentra al Este de nuestro pais";
+			break;
+		case"Ushuaia":
+			mensaje = "Se encuentra al Sur de nuestro pais";
+			break;
+
+	}
+
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
