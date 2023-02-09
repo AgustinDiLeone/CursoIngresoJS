da1c2683b12f061027abef092ebc020b37d82953
AgustinDiLeone <agustindileone@gmail.com>
2023-02-09 11:54:59 -0300

    listo

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,26 @@
+//Agustin Di Leone
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mesDelAño;
+	let mensaje;
 
+	mesDelAño = document.getElementById("txtIdMes").value; 
 
+	switch(mesDelAño){
+		case"Abril":
+		case"Junio":
+		case"Septiembre":
+		case"Noviembre":
+			mensaje = "Tiene 30 dias";
+			break;
+		case"Febrero":
+			mensaje = "Tiene 28 dias";
+			break;
+		default:
+			mensaje = "Tiene 31 dias";
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
