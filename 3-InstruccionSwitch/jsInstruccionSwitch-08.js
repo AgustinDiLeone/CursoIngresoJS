9f862c8a44a684613c69f5a4ee515f65341169c3
AgustinDiLeone <agustindileone@gmail.com>
2023-02-09 12:34:01 -0300

    listo

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,28 @@
+//Agustin Di Leone
+
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destinoIngresado;
+	let mensaje;
+
+	destinoIngresado = document.getElementById("txtIdDestino").value;
+
+	switch(destinoIngresado){
+		case"Bariloche":
+		mensaje = "Hace frio";
+		break;
+	case"Cataratas":
+		mensaje = "Hace calor";
+		break;
+	case"Mar del plata":
+		mensaje = "Hace calor";
+		break;
+	case"Ushuaia":
+		mensaje = "Hace frio";
+		break;
+
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
