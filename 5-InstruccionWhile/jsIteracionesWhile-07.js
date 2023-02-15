afcaa245657014d08625255fe9f0f8049b81c61c
AgustinDiLeone <agustindileone@gmail.com>
2023-02-15 11:06:37 -0300

    listo

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -1,4 +1,4 @@
-/*
+/*Agustin Di Leone
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -1,19 +1,36 @@
-/*
+/*Agustin Di Leone
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
+	let numero;
+	let acumuladorSuma;
+	let respuesta;
+	let multiplicacionNegativos;
+
+	acumuladorSuma = 0;
+	multiplicacionNegativos = 1;
 	respuesta='si';
 
+	while(respuesta == "Si" || respuesta == "SI" || respuesta == "si" ){
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+		numero = parseInt(prompt("Ingrese un numero"));
+
+		while(isNaN(numero)){
+			numero = parseInt(prompt("Error: Ingrese un numero"));
+		}
+		if(numero > 0){
+
+			acumuladorSuma = acumuladorSuma + numero;
+		}else{
+
+			multiplicacionNegativos = multiplicacionNegativos * numero;
+		}
+		
+		respuesta = prompt("deseas ingresar otro numero? SI/NO");
+	}
+	
+	document.getElementById("txtIdSuma").value = acumuladorSuma;
+	document.getElementById("txtIdProducto").value = multiplicacionNegativos;
 
 }//FIN DE LA FUNCIÓN
