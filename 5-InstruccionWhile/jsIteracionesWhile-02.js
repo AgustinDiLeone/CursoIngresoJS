4238d7de2c54fd27dcecc7d2b21b344b204094f0
AgustinDiLeone <agustindileone@gmail.com>
2023-02-16 11:07:57 -0300

    listo

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -21,9 +21,50 @@ function mostrar()
 // while 2 bis: Ingresar 5 números, y determinar la cantidad de números que son mayores que 10 
 //y menores 20 (inclusive en ambos casos)
 
+function mostrar()
+{
+	let numero;
+	let cantidad;
+	
+	for(let i = 0; i > 5; i++){
+		numero = parseFloat(prompt("Ingrese un numero"));
+
+		while(isNaN(numero)){
+			numero = parseFloat(prompt("Error: Ingrese un numero valido"))
+		}
+	}if(numero >= 10 && numero <= 20){
+		cantidad = cantidad + 1;
+
+	}
+		
+	console.log("Ingresaste " + cantidad + " de numeros entre el 10 y el 20 ")
+}
 
 
 /*While 2 bis bis: Ingresar numeros hasta que el usuario quiera. 
 Determinar la cantidad de números que son mayores que 10 
 y menores 20 (inclusive en ambos casos). 
 También el promedio de los números que cumplen la condición anterior.*/
+/*
+function mostrar()
+{
+	let numero;
+	let respuesta;
+	let contador;
+	let promedio;
+
+	contador = 0;
+	respuesta='si';
+
+	while(respuesta == "Si" || respuesta == "SI" || respuesta == "si" ){
+
+		numero = parseInt(prompt("Ingrese un numero"));
+
+		while(isNaN(numero)){
+			numero = parseInt(prompt("Error: Ingrese un numero"));
+		}
+		
+		
+		respuesta = prompt("deseas ingresar otro numero? SI/NO");
+	}
+}*/
--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,6 +1,7 @@
 /*Agustin Di Leone
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
+/*
 function mostrar()
 {	// declarar variables
 	let banderaDelPrimero;
@@ -38,4 +39,69 @@ function mostrar()
 	}
 	document.getElementById("txtIdMaximo").value = numeroMaximo;
 	document.getElementById("txtIdMinimo").value = numeroMinimo
+}*/
+
+/*
+While 9 bis: Al presionar el botón pedir  números  hasta que el usuario quiera,
+mostrar el número máximo y el número mínimo ingresado.
+El menor de los pares y el mayor de los negativos ...(solo si hay)*/
+
+function mostrar()
+{
+	let numero;
+	let respuesta;
+	let bandera;
+	let maximo;
+	let minimo;
+	let banderaPar;
+	let menorPar;
+	let banderaNegativo; 
+	let maximoNegativo;
+
+	respuesta='si';
+	bandera = true;
+	banderaPar = true;
+	banderaNegativo = true;
+
+	while(respuesta == "Si" || respuesta == "SI" || respuesta == "si" ){
+
+		numero = parseInt(prompt("Ingrese un numero"));
+
+		while(isNaN(numero)){
+			numero = parseInt(prompt("Error: Ingrese un numero"));
+		}
+		if(bandera){
+
+			maximo = numero;
+			minimo = numero;
+			bandera = false
+
+		}else if(numero > maximo){
+
+			maximo = numero;
+
+		}else if(numero < minimo){
+
+			minimo = numero;
+		}
+		if(numero % 2 == 0){
+			if(banderaPar || menorPar > numero){
+				menorPar = numero;
+				banderaPar = false;
+			}
+		}
+		if(numero < 0){
+			if(banderaNegativo || maximoNegativo < numero){
+				maximoNegativo = numero;
+				banderaNegativo = false;
+			}
+		}
+		
+		respuesta = prompt("deseas ingresar otro numero? SI/NO");
+	}
+
+	document.write("El numero maximo es: " + maximo + "<br>");
+	document.write("El numero minimo es: " + minimo + "<br>");
+	document.write("El menor numero par es: "+ menorPar + "<br>");
+	document.write("El maximo numero negativo es: " + maximoNegativo);
 }
