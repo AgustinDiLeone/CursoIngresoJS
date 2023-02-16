985e402e0c9123da9c861c9bed308c90aecdcae1
AgustinDiLeone <agustindileone@gmail.com>
2023-02-16 13:18:34 -0300

    listo

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,27 @@
 function mostrar()
 {
+	let contadorDivisores;
+	let numeroIngresado;
 
-	alert("ok");
+	contadorDivisores = 0;
 
+	numeroIngresado = parseInt(prompt("Ingresa un numero"));
+		while(isNaN(numeroIngresado)){
+			numeroIngresado = parseInt(prompt("Error: Ingresa un numero"));
+
+		}
+
+	for(let i = 0; i < numeroIngresado; i++){
+				
+		if(numeroIngresado % i == 0){
+
+			console.log("Los numeros divisores son: " + i);
+			contadorDivisores = contadorDivisores + 1;
+
+		}
+
+	}
+
+	console.log("Hay: " + contadorDivisores + " numeros divisores")
 
 }//FIN DE LA FUNCIÓN
