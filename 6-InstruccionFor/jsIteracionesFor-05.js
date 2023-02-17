4343294f9b32432914682230396e55faab88bbcc
AgustinDiLeone <agustindileone@gmail.com>
2023-02-16 21:41:36 -0300

    listo

--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -1,3 +1,4 @@
+/*
 function mostrar()
 {
 	let repetir;
@@ -11,4 +12,74 @@ function mostrar()
 	}
 
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN*/
+
+/*For 5 bis, Realizar el programa que permita el ingreso de las notas (validar entre 0 y 10) , 
+el sexo (validar el sexo “f” o “m” o "b") de 7 alumnos, informar por console.log: 
+a) El promedio de las notas totales. 
+b) La nota más baja y el sexo de esa persona. 
+c) La cantidad de varones que su nota haya sido mayor o igual a 6.
+d) La cantidad de alumnos desaprobados (nota < 4)*/
+
+	function mostrar()
+	{
+		let notas;
+		let sexo;
+		let acumulador;
+		let cantidad;
+		let promedio;
+		let banderaMenor;
+		let notaBaja;
+		let sexoBajo;
+		let contadorMasculino;
+		let cantidadDesaprobados;
+
+		acumulador = 0;
+		cantidad = 0;
+		banderaMenor = true;
+		contadorMasculino = 0;
+		cantidadDesaprobados = 0;
+
+
+		for(let i = 0; i < 7; i++){
+			notas = parseInt(prompt("Ingrese su nota"));
+				while(notas < 0 || notas > 10){
+
+					notas = parseInt(prompt("Error: Ingrese su nota"));
+				}
+			sexo = prompt('Ingrese su sexo “f” o “m” o "b"');
+				while(!(sexo == "f" || sexo == "m" || sexo == "b")){
+
+					sexo = prompt('Error: Ingrese su sexo “f” o “m” o "b"')
+				}
+			acumulador = acumulador + notas;
+			cantidad = cantidad + 1;
+			promedio = acumulador / cantidad;
+
+			if(banderaMenor){
+				notaBaja = notas;
+				sexoBajo = sexo;
+				banderaMenor = false;
+
+			}else if(notas < notaBaja){
+
+				notaBaja = notas;
+				sexoBajo = sexo;
+			}
+			if(sexo == "m" && notas >= 6){
+
+				contadorMasculino = contadorMasculino + 1;
+			}
+			if(notas < 4){
+
+				cantidadDesaprobados = cantidadDesaprobados + 1;
+			}
+
+		}
+
+	
+		console.log("El promedio de las notas es: " + promedio);
+		console.log("La nota mas baja es un: " + notaBaja + " y esta nota es de un: " + sexoBajo);
+		console.log("La cantidad de alumnos masculinos promocionados son:" + contadorMasculino)
+		console.log("La cantidad de desaprobados fue: " + cantidadDesaprobados)
+	}
--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -10,10 +10,50 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
 function ComenzarIngreso () 
 {
  	//definicion de variables
- 	var edadIngresada;
- 	var sexoIngresado;
- 	var estadoCivilIngresado;
+ 	let edadIngresada;
+ 	let sexoIngresado;
+ 	let estadoCivilIngresado;
+	let sueldoBruto;
+	let legajo;
+	let nacionalidad;
+
+	edadIngresada = parseInt(prompt("Ingrese su edad"))
+		while(isNaN(edadIngresada) || edadIngresada < 0 || edadIngresada > 120){
+
+			edadIngresada = parseInt(prompt("Error: Ingrese su edad"));
+		}
+	sexoIngresado = prompt("Ingrese su sexo: M(masculino) o F(femenino)")
+		while(!( sexoIngresado == "F" || sexoIngresado == "M")){
+
+			sexoIngresado = prompt("Error: Ingrese su sexo: M(masculino) o F(femenino)")
+		}
+	estadoCivilIngresado = prompt("Ingrese su Estado civil: 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos")
+		while(isNaN(estadoCivilIngresado) || estadoCivilIngresado < 1 || estadoCivilIngresado > 4){
+
+			estadoCivilIngresado = prompt("Error: Ingrese su> Estado civil: 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos")
+		}
+	sueldoBruto = parseInt(prompt("Ingrese su sueldo bruto"))
+		while(isNaN(sueldoBruto) || sueldoBruto < 8000 || sueldoBruto > 100000000){
+
+			sueldoBruto = parseInt(prompt("Error: Ingrese su sueldo bruto"));
+		}
+	legajo = parseInt(prompt("Ingrese su numero de legajo"))
+		while(isNaN(legajo) ||legajo < 1000 || legajo >= 10000){
+
+			legajo = parseInt(prompt("Error: Ingrese su numero de legajo"));
+		}
+	nacionalidad = prompt("Ingrese su Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados")
+		while(!( nacionalidad == "A" || nacionalidad == "E" || nacionalidad == "N" )){
+
+			nacionalidad = prompt("Error: Ingrese su Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados")
+		}
 
 
+	 document.getElementById("txtIdEdad").value = edadIngresada
+	 document.getElementById("txtIdSexo").value = sexoIngresado
+	 document.getElementById("txtIdEstadoCivil").value = estadoCivilIngresado
+	 document.getElementById("txtIdSueldo").value = sueldoBruto
+	 document.getElementById("txtIdLegajo").value = legajo
+	 document.getElementById( "txtIdNacionalidad").value = nacionalidad
 
 }
