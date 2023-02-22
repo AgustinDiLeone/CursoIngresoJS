91642c8c5b325fb5969417fa8c21a71d6f1f9d04
AgustinDiLeone <agustindileone@gmail.com>
2023-02-21 22:50:47 -0300

    listo

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,85 @@
+/*Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo con mas cantidad de bolsas.
+f) El tipo mas caro
+*/
+
 function mostrar()
 {
-  alert("dos");
+  let producto;
+  let cantidad;
+  let precio;
+  let descuento;
+  let precioBruto;
+  let precioFinal;
+  let precioTotalFinal;
+  let precioConjunto;
+  let cantidadMayor;
+  let bolsaCara;
+  let productoCaro;
+  let productoMayor;
+
+  respuesta = "si";
+  precioBruto = 0;
+  precioTotalFinal = 0;
+  cantidadMayor = 0;
+  bolsaCara = 0;
+
+
+  while(respuesta == "Si" || respuesta == "SI" || respuesta == "si" ){
+    producto = prompt("Ingrese un producto de construccion");
+      while(!(producto == "arena" || producto == "cal" || producto == "cemento")){
+        producto = prompt("Error: Ingrese un producto de construccion");
+      }
+    cantidad = parseInt(prompt("Ingrese la cantidad del producto"));
+      while(isNaN(cantidad)){
+        cantidad = parseInt(prompt("Error: Ingrese la cantidad del producto"));
+      }
+    precio = parseFloat(prompt("Ingrese el precio del producto"));
+      while(isNaN(cantidad)){
+        precio = parseFloat(prompt("Eror: Ingrese el precio del producto"));
+      }
+      if(cantidad > 30){
+        descuento = 25
+
+      }else if(cantidad > 10){
+          descuento = 15
+
+      }else{
+        descuento = 0
+      }
+      precioConjunto = precio * cantidad;
+      precioBruto = precioBruto + precioConjunto;
+      precioFinal = precioConjunto - (precioConjunto * descuento / 100);
+      precioTotalFinal = precioTotalFinal + precioFinal;
+
+      if(cantidad > cantidadMayor){
+
+        cantidadMayor = cantidad;
+        productoMayor = producto;
+
+      }
+      if(bolsaCara < precio){
+
+        bolsaCara = precio;
+        productoCaro = producto;
+
+      }
+
+
+      respuesta = prompt("deseas ingresar otro producto? SI/NO");
+  }
+
+  console.log("El importe total sin descuento es: " + precioBruto);
+  console.log("El importe total con descuento es: " + precioTotalFinal);
+  console.log("El tipo con mas cantidad de bolsas es: " + productoMayor);
+  console.log("El tipo mas caro es: " + productoCaro);
+
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,85 @@
+/* Debemos realizar la carga de 5(cinco) productos de prevención de contagio,
+de cada una debo obtener los siguientes datos:
+el tipo (validar "barbijo" , "jabón" o "alcohol") ,
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+la Marca y el fabricante.
+Se debe Informar al usuario lo siguiente:
+a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+b) Del tipo con mas unidades, el promedio por compra
+c) Cuántas unidades de jabones hay en total
+*/
 
 function mostrar()
 {
-	alert("uno");
+	let producto;
+	let precio;
+	let cantidad;
+	let marca;
+	let fabricante;
+	let banderaAlcohol;
+	let cantidadAlcoholBarato;
+	let fabricanteAlcoholBarato;
+	let precioAlcoholBarato;
+	let banderaCantidad;
+	let cantidadProducto;
+	let promedio;
+	let cantidadJabon;
+
+	banderaAlcohol = true;
+	cantidadAlcoholBarato = 0;
+	fabricanteAlcoholBarato = "No hay";
+	precioAlcoholBarato = 0;
+	banderaCantidad = true;
+	cantidadProducto = 0;
+	cantidadJabon  = 0;
+
+	for(let i = 0; i < 5; i++){
+		producto = prompt("Ingrese un producto de prevención de contagio");
+			while(!(producto == "barbijo" || producto == "jabon" || producto == "alcohol")){
+
+				producto = prompt("Error: Ingrese un producto de prevención de contagio");
+			}
+		precio = parseFloat(prompt("Ingrese el precio del producto"));
+			while(isNaN(precio) || precio < 100 || precio > 300){
+
+				precio = parseFloat(prompt("Error: Ingrese el precio del producto"));
+			}
+		cantidad = parseInt(prompt("Ingrese la cantidad del producto"));
+			while(isNaN(cantidad) || cantidad < 1 || cantidad > 1000){
+
+				cantidad = parseInt(prompt("Error: Ingrese la cantidad del producto"));
+			}
+		marca = prompt("Ingrese la marca del producto");
+		fabricante = prompt("Ingrese el fabricante del producto");
+
+		if(banderaAlcohol && producto == "alcohol"){
+			precioAlcoholBarato = precio;
+			cantidadAlcoholBarato = cantidad;
+			fabricanteAlcoholBarato = fabricante;
+			banderaAlcohol = false;
+			
+		}else if(producto == "alcohol" && precio < precioAlcoholBarato){
+			precioAlcoholBarato = precio;
+			cantidadAlcoholBarato = cantidad;
+			fabricanteAlcoholBarato = fabricante;
+			
+		}
+		if(cantidadProducto < cantidad){
+			cantidadProducto = cantidad;
+			promedio = precio / cantidadProducto;
+
+		}
+		if(producto == "jabon"){
+
+			cantidadJabon = cantidadJabon + cantidad;
+		}
+
+		
+	}
+
+	console.log("El alcohol mas barato es: " + fabricanteAlcoholBarato + " comprando " + cantidadAlcoholBarato + " unidades");
+	console.log("El promedio de la compra con mayor unidades es: " + promedio);
+	console.log("Hay " + cantidadJabon + " unidades de jabon en total");
+
 }
