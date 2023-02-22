7f79b3bd5aa3a6c714f834631aec4c6d6b845d03
AgustinDiLeone <agustindileone@gmail.com>
2023-02-22 09:08:44 -0300

    listo

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,5 +1,32 @@
 function mostrar()
 {
+	let contadorDivisores;
+	let numeroIngresado;
 
+	contadorDivisores = 0;
+
+	numeroIngresado = parseInt(prompt("Ingresa un numero"));
+		while(isNaN(numeroIngresado)){
+			numeroIngresado = parseInt(prompt("Error: Ingresa un numero"));
+
+		}
+        
+	for(let i = 1; i < numeroIngresado; i++){
+				
+		if(numeroIngresado % i == 0){
+
+		    contadorDivisores = contadorDivisores + 1;
+
+		}
+        if(contadorDivisores == 2){
+
+		    console.log(numeroIngresado + " es primo");
+
+		}else{
+
+            console.log(numeroIngresado + " no es primo");
+        }
+
+	}
 
 }//FIN DE LA FUNCIÓN
